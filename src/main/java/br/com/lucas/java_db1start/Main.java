64144c1fd23000cacb5f4c04784e22d4c2ed4931
Lucas Ribeiro Barzotto <lucasribeirobarzotto@gmail.com>
2021-05-04 00:52:49 -0300

    Add PasswordMeterV1

--- src/main/java/br/com/lucas/java_db1start/Main.java
+++ src/main/java/br/com/lucas/java_db1start/Main.java
@@ -3,7 +3,12 @@ package br.com.lucas.java_db1start;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        PasswordMeterV1 myPassword = new PasswordMeterV1();
+
+        myPassword.checkPassword("a1b5d4s8d4s");
+        System.out.println(myPassword);
+
     }
 
 }
